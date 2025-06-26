445dab071194a936338e7b9fe184fc22cbbf479a
yapa5475 <yash.parekh223@gmail.com>
2025-06-25 23:48:57 -0600

    TodoList conditional button hiding and test

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,24 +1,31 @@
 import { TodoItem } from "./TodoItem"
 
 export function TodoList({ todos, toggleTodo, deleteTodo, duplicateTodo, markAllAsCompleted, deleteAllTodos }) {
+  const hasTodos = todos.length > 0
+
   return (
     <>
-    
-    <button className="btn" onClick={markAllAsCompleted}>Mark All as Completed</button>
-    <button className="btn" onClick={deleteAllTodos}>Delete All</button>
-    
-    <ul className="list">
-      {todos.length === 0 && "No Todos"}
-      {[...todos]
-        .sort((a, b) => a.completed - b.completed)
-        .map(todo => (
-          <TodoItem
-            {...todo}
-            key={todo.id}
-            toggleTodo={toggleTodo}
-            deleteTodo={deleteTodo}
-            duplicateTodo={duplicateTodo} />
-        ))}
-    </ul></>
+      {hasTodos && (
+        <div className="button-group">
+          <button className="btn" onClick={markAllAsCompleted}>Mark All as Completed</button>
+          <button className="btn btn-danger btn-hover" onClick={deleteAllTodos}>Delete All</button>
+        </div>
+      )}
+
+      <ul className="list">
+        {!hasTodos && "No Todos"}
+        {[...todos]
+          .sort((a, b) => a.completed - b.completed)
+          .map(todo => (
+            <TodoItem
+              {...todo}
+              key={todo.id}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+              duplicateTodo={duplicateTodo}
+            />
+          ))}
+      </ul>
+    </>
   )
 }
