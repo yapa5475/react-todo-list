9a725563a3f723ab004ff4e4a3f680e8a67d09b9
yapa5475 <yash.parekh223@gmail.com>
2025-06-28 12:37:55 -0600

    ip

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "cypress";
+import { b } from "vitest/dist/chunks/suite.d.FvehnV49.js";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    baseUrl: "http://localhost:5179",
+    // setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+    // },
   },
 });
