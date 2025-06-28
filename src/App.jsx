3f65f78080a9c37b0d12fbf814c15ffb5574f111
yapa5475 <yash.parekh223@gmail.com>
2025-06-28 12:16:58 -0600

    reorder

--- src/App.jsx
+++ src/App.jsx
@@ -75,9 +75,9 @@ export default function App() {
 
   return (
     <>
-      <NewTodoForm onSubmit={addTodo} />
       <h1 className="header">Todo List</h1>
       <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} duplicateTodo={duplicateTodo} markAllAsCompleted={markAllAsCompleted} deleteAllTodos={deleteAllTodos} />
+      <NewTodoForm onSubmit={addTodo} />
     </>
   )
 }
